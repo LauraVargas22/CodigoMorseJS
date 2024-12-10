d1d30b9b5048329c770f7824b9bdbe3fe41af1ed
Laura <181015028+LauraVargas22@users.noreply.github.com>
2024-12-10 08:25:52 -0500

    feat: :zap: Codigo Morse Modificaciòn en ek README

--- js/java.js
+++ js/java.js
@@ -1,5 +1,5 @@
 const dictionary = {
-    a: ".-", b: "-...", c: "-.-.", d: "-..", e: ".", f: "..-.", g: "--.", h: "....", i: "..", j: ".---", k: "-.-", l: ".-..", m: "--", n: "-.", ñ: "--.--", o: "---", p: ".--.", q: "--.-", r: ".-.", s: "...", t: "-", u: "..-", v: "...-", w: ".--", x: "-..-", y: "-.--", z: "--..", 0: "-----", 1: ".----", 2: "..---", 3: "...--", 4: "....-", 5: ".....", 6: "-....", 7: "--...", 8: "---..", 9: "----.", ".": ".-.-.-", ",": "--..--", "?": "..--..", "/": "-..-.",
+    "a": ".-", b: "-...", c: "-.-.", d: "-..", e: ".", f: "..-.", g: "--.", h: "....", i: "..", j: ".---", k: "-.-", l: ".-..", m: "--", n: "-.", ñ: "--.--", o: "---", p: ".--.", q: "--.-", r: ".-.", s: "...", t: "-", u: "..-", v: "...-", w: ".--", x: "-..-", y: "-.--", z: "--..", 0: "-----", 1: ".----", 2: "..---", 3: "...--", 4: "....-", 5: ".....", 6: "-....", 7: "--...", 8: "---..", 9: "----.", ".": ".-.-.-", ",": "--..--", "?": "..--..", "/": "-..-.",
 };
 
 function translateCodMorse () {
@@ -17,6 +17,24 @@ function translateCodMorse () {
     }
 
     translateText.textContent = `El codigo Morse es: ${translateMorse}`;
+}
+
+function translateTexttoMorse () {
+    const userCode = document.getElementById('userCode').value.toLowerCase();
+    const translateCod = document.getElementById('translateCod');
+
+    let translateText = "";
+    const morseCode = userCode.split("");
+
+    for (let i = 0; i < userCode.length; i++) {
+        const symbols = morseCode[i];
+        translateText += dictionary[symbols] || "?";
+        if (i < userCode.length - 1) {
+            translateText += "";
+        }
+    }
+
+    translateCod.textContent = `El texto es: ${translateText}`;
 }
 
 
